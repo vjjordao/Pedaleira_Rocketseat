9f506e9db10735adb46f453a1fa46a44b6fc590f
vjjordao <54770871+vjjordao@users.noreply.github.com>
2020-05-03 11:36:46 -0300

    Visual basic resources

---
+++ Visual Basic/PedaleiraRocketseat/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.cbb_portas = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.bt_conectar = New System.Windows.Forms.Button()
+        Me.bt_Desconectar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cbb_portas
+        '
+        Me.cbb_portas.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cbb_portas.FormattingEnabled = True
+        Me.cbb_portas.Location = New System.Drawing.Point(12, 25)
+        Me.cbb_portas.Name = "cbb_portas"
+        Me.cbb_portas.Size = New System.Drawing.Size(133, 21)
+        Me.cbb_portas.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Selecione uma porta"
+        '
+        'bt_conectar
+        '
+        Me.bt_conectar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.bt_conectar.Location = New System.Drawing.Point(151, 11)
+        Me.bt_conectar.Name = "bt_conectar"
+        Me.bt_conectar.Size = New System.Drawing.Size(89, 23)
+        Me.bt_conectar.TabIndex = 2
+        Me.bt_conectar.Text = "Conectar"
+        Me.bt_conectar.UseVisualStyleBackColor = True
+        '
+        'bt_Desconectar
+        '
+        Me.bt_Desconectar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.bt_Desconectar.Location = New System.Drawing.Point(151, 36)
+        Me.bt_Desconectar.Name = "bt_Desconectar"
+        Me.bt_Desconectar.Size = New System.Drawing.Size(89, 23)
+        Me.bt_Desconectar.TabIndex = 3
+        Me.bt_Desconectar.Text = "Desconectar"
+        Me.bt_Desconectar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(249, 64)
+        Me.Controls.Add(Me.bt_Desconectar)
+        Me.Controls.Add(Me.bt_conectar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cbb_portas)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Pedaleira RocketSeat"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cbb_portas As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents bt_conectar As Button
+    Friend WithEvents bt_Desconectar As Button
+End Class
---
+++ Visual Basic/PedaleiraRocketseat/Form1.vb
@@ -0,0 +1,110 @@
+﻿Imports System
+Imports System.Threading
+Imports System.ComponentModel
+Imports System.IO.Ports
+
+
+
+Public Class Form1
+    Public WithEvents port As New clsSynchronizedSerialPort()
+
+    'this declares the user32.dll call to SendMessageW we are making
+    Declare Auto Function SendMessageW Lib "user32.dll" Alias "SendMessageW" (
+    ByVal hWnd As Integer,
+    ByVal Msg As Integer,
+    ByVal wParam As Integer,
+    ByVal lParam As Integer) As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            cbb_portas.Items.Add(sp)
+        Next
+    End Sub
+
+    Private Sub bt_conectar_Click(sender As Object, e As EventArgs) Handles bt_conectar.Click
+        port.open(cbb_portas.SelectedItem, 9600)
+    End Sub
+    Private Sub cont_MsgReceived(ByVal msgString As String) Handles port.MsgReceived
+        'lbl_status.Text = SerialPort1.ReadExisting
+        'this constant represents the hex value for the key to send to user32.dll
+        Const APPCOMMAND_MEDIA_PLAY_PAUSE = &HE0000
+        'this constant represents which command. Sort of like the function in user32.dll we are calling.
+        Const WM_APPCOMMAND = &H319
+        SendMessageW(Handle, WM_APPCOMMAND, Handle, APPCOMMAND_MEDIA_PLAY_PAUSE)
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+
+    Private Sub bt_Desconectar_Click(sender As Object, e As EventArgs) Handles bt_Desconectar.Click
+        port.close()
+    End Sub
+End Class
+
+
+
+Public Class clsSynchronizedSerialPort
+
+    Private WithEvents Connection As New SerialPort
+    Public Event MsgReceived(ByVal msg As String)
+    Private asynOp As AsyncOperation
+
+    Public Sub New()
+
+        asynOp = AsyncOperationManager.CreateOperation(Nothing)
+
+    End Sub
+    Public Sub open(ByVal portName As String, ByVal baud As Integer)
+
+        'asynOp = AsyncOperationManager.CreateOperation(Nothing)
+        Try
+            With Connection
+                .PortName = portName
+                .BaudRate = baud
+                .Parity = Parity.None
+                .DataBits = 8
+                .StopBits = StopBits.One
+            End With
+            Connection.Open()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+
+        End Try
+    End Sub
+
+    Public Sub close()
+        Connection.Close()
+    End Sub
+
+
+    Private Sub Connection_DataReceived(ByVal sender As Object,
+            ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles Connection.DataReceived
+
+        Dim MsgString As String = Nothing
+        If Connection.BytesToRead > 0 Then
+            Do
+                MsgString &= Chr(Connection.ReadByte)
+                If Connection.BytesToRead = 0 Then
+                    Exit Do
+                End If
+            Loop
+            asynOp.Post(New SendOrPostCallback(AddressOf CallbackDelegate), MsgString)
+        End If
+
+
+    End Sub
+
+    Private Sub CallbackDelegate(ByVal MsgString As Object)
+
+        RaiseEvent MsgReceived(CStr(MsgString))
+
+    End Sub
+
+    Public Sub SendMsg(ByVal msg As String)
+
+        Connection.Write(msg)
+
+    End Sub
+
+End Class
